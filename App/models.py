ac713cb344f26b498918ab2ff243a8e4d153038d
AtifZawadBakshi <atif.zawad4432@gmail.com>
2022-07-26 10:35:58 +0600

    before database

--- App/models.py
+++ App/models.py
@@ -5,26 +5,20 @@ import datetime
 
 class Category(models.Model):    
 
-    category_name=models.CharField(max_length=100, unique=True) 
-    
+    category_name=models.CharField(max_length=100, unique=True)
+
     def __str__(self):
-        return self.category_name 
-
-class SubCategory(models.Model):    
-
+        return self.category_name
+class SubCategory(models.Model):
     category=models.ForeignKey(Category,on_delete=models.CASCADE) 
-    sub_category_name=models.CharField(max_length=100, unique=True) 
-    
+    sub_category_name=models.CharField(max_length=100, unique=True)
     def __str__(self):
-        return self.sub_category_name 
-
-class Product(models.Model):   
-     
+        return self.sub_category_name
+class Product(models.Model):
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
     sub_category=models.ForeignKey(SubCategory,on_delete=models.CASCADE,null=True) 
     product_name=models.CharField(max_length=100, unique=True, null=True) 
-    regular_price=models.FloatField(max_length=100, unique=True, null=True) 
-    #discounted_price=models.FloatField(max_length=100, unique=True, null=True) 
-  
+    regular_price=models.FloatField(max_length=100, unique=True, null=True)
+
     def __str__(self):
         return self.product_name
